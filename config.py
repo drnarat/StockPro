248f75ff6ddd3eda80768fdc1a4d87896f2e8f31
drnarat <dr.narat@gmail.com>
2026-04-25 08:51:04 +0700

    Remove sensitive configuration data from config.py

--- config.py
+++ config.py
@@ -1,11 +1,1 @@
-```python
-# config.py
-SETTRADE_CONFIG = {
-    "APP_ID": "MPRZz1Hymo6nR50A",
-    "APP_SECRET": "Te/3LKXBb+IM20T/ygcFAMWXjIgkadJ+o1cDstkjRDQ=",
-    "APP_CODE": "SANDBOX",
-    "BROKER_ID": "SANDBOX",
-    "ACCOUNT_NO": "Narats-E",
-    "PIN": "111111",
-    "IS_SANDBOX": True
-}
+
